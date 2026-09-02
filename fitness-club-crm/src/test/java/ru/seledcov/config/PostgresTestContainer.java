7929cee5591271db7995dea754b600220557a5c1
Aleksey Seledcov <alex-ozu@yandex.ru>
2026-09-02 16:17:39 +0300

    test: add integration test infrastructure

---
+++ fitness-club-crm/src/test/java/ru/seledcov/config/PostgresTestContainer.java
@@ -0,0 +1,33 @@
+package ru.seledcov.config;
+
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import ru.seledcov.infrastructure.DatabaseCleaner;
+
+@Testcontainers
+public abstract class PostgresTestContainer {
+
+    @Container
+    protected static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:18.6");
+
+    @DynamicPropertySource
+    private static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @AfterEach
+    void cleanDatabase() {
+        databaseCleaner.cleanup();
+    }
+}
